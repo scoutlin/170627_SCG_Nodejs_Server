c27f51e38d425c203358ba93ab0a8896aa4cc794
DESKTOP-MGHFEMN\ScottLin <scoutlinn@gmail.com>
2017-06-29 18:18:13 +0800

    Yeah~~~~

--- 170627_SCG_Nodejs_Server/170627_SCG_Nodejs_Server/app.js
+++ 170627_SCG_Nodejs_Server/170627_SCG_Nodejs_Server/app.js
@@ -5,26 +5,83 @@ console.log('Hello world')
 
 let express = require('express')
 let edge = require('edge')
+let bodyPaser = require('body-parser')
 
 let app = express()
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.use(bodyPaser.json())
+app.use(bodyPaser.urlencoded({ extended: false }))
 
-app.listen(3000)
-
+//let helloWorld = edge.func(`
+//    async (input) => { 
+//        return ".NET Welcomes " + input.ToString(); 
+//    }
+//`);
 
 
 let helloWorld = edge.func(`
-    async (input) => { 
-        return ".NET Welcomes " + input.ToString(); 
+    using System.Threading.Tasks;
+    
+    public class Startup
+    {
+        public async Task<object> Invoke(object input)
+        {
+            Counter mCounter = Counter.GetInstance();
+
+            mCounter.fuck++;
+
+            return mCounter.fuck;
+        }
+    }
+
+    public class Counter
+    {
+        public static Counter mCounter;
+        public int fuck = "";
+        
+        public static GetInstance()
+        {
+            if(mCounter == null)
+            {
+                mCounter = new Counter();
+            }
+            
+            return mCounter;
+        }
     }
 `);
 
-helloWorld('JavaScript', function (error, result) {
-    if (error) throw error;
-    console.log(result);
+app.post('/HelloWorld', function (req, res) {
+
+    helloWorld('JavaScript', function (error, result) {
+        if (error) throw error;
+        console.log(result);
+    });
+
+    res.end(result);
+
 });
 
 
-console.log('Yeah')
+
+    app.post('/ServerVersion', function (req, res) {
+
+        console.log("req.url: " + req.url);
+        console.log("HEADER: " + req.body.HEADER);
+        console.log("JSON: " + req.body.JSON);
+
+        let Package_Version = { "version": "1.0.0", "bundle": "0" }
+        let Packet_Version_JSON = JSON.stringify(Package_Version)
+        console.log("Packet_Version_JSON: " + Packet_Version_JSON)
+
+        //To Object
+        //JSON.parse()
+        //To String
+        //JSON.stringify()
+
+        //res.send('Hello World')
+        res.end(Packet_Version_JSON);
+    });
+
+    app.listen(3000);
+
+    console.log('Yeah');
