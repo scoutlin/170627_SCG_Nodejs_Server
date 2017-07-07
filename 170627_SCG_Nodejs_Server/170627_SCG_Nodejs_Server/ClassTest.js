d4bfa322b24fca63465a83970bbce609935b3f95
scott <scottlin@jumbogames.com.tw>
2017-07-08 01:01:59 +0800

    Fix the class is not constructor error by don`t know why

---
+++ 170627_SCG_Nodejs_Server/170627_SCG_Nodejs_Server/ClassTest.js
@@ -0,0 +1,34 @@
+﻿'use strict'
+
+console.log('fdfsdfdsfdsfwf')
+
+
+
+class ClassTest {
+    constructor(yeah) {
+        this.yeah = 0;
+        this.classTest = null
+    }
+
+    static get Instance(){
+        if (ClassTest.classTest == null) {
+            ClassTest.classTest = new ClassTest()
+        }
+        
+        return ClassTest.classTest
+    }
+
+    OhYeah(yeah) {
+        console.log(yeah);
+    }
+
+    static StaticOhYeah(yeah) {
+        console.log(yeah);
+    }
+}
+
+ClassTest.staticVariable001 = 'S1';
+
+module.exports = ClassTest
+
+console.log('fdfsdfdsfdsfwf')
--- 170627_SCG_Nodejs_Server/170627_SCG_Nodejs_Server/app.js
+++ 170627_SCG_Nodejs_Server/170627_SCG_Nodejs_Server/app.js
@@ -2,8 +2,23 @@
 console.log('app Start')
 
 //-------------------local-----------------------------
+let ClassTest = require('./ClassTest')
 let CommonSetting = require('./CommonSetting')
 let expressModule = require('./module/express-module')
 //-----------------------------------------------------
+
+let mClassTest = new ClassTest()
+mClassTest.OhYeah('!!!!!!!!!!!!!!YEAH!!!!!!!!!!!!!!!')
+ClassTest.StaticOhYeah('Static Yeah')
+console.log(ClassTest.staticVariable001)
+
+
+let mClassTest2 = ClassTest.Instance
+mClassTest2.yeah++
+console.log('yeah: ' + mClassTest2.yeah);
+
+let mClassTest3 = ClassTest.Instance
+mClassTest3.yeah++
+console.log('yeah: ' + mClassTest3.yeah)
 
 console.log('Yeah')
